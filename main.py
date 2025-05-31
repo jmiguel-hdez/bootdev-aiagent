da8be6fbaf62b8f41e342a03dc46cf36cea0951d
jmiguel-hdez <jmiguel.hdez@gmail.com>
2025-05-31 16:49:40 -0500

    use generate_content function

--- main.py
+++ main.py
@@ -22,16 +22,16 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
 
     client = genai.Client(api_key=api_key)
+    generate_content(client, messages)
+
+
+def generate_content(client, messages):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
     )
-
-    if response:
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-        print("Response:")
-        print(response.text)
+    print("Response:")
+    print(response.text)
 
 
 if __name__ == "__main__":
