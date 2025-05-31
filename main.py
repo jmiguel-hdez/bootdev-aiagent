72ed635eb99373107bc63c61d97a3b2430746a6a
jmiguel-hdez <jmiguel.hdez@gmail.com>
2025-05-31 17:00:17 -0500

    add verbose flag, simple implementation without argparse

--- main.py
+++ main.py
@@ -4,19 +4,26 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+verbose = False
+
 
 def main():
+    global verbose
     if len(sys.argv) < 2:
         print("AI Code Assistant")
         print('\nUsage: python main.py "your prompt here"')
         print('Example: python main.py "How do I build a calculator app?"')
         sys.exit(1)
 
-    args = sys.argv[1:]
-    user_prompt = " ".join(args)
+    user_prompt = sys.argv[1]
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
+    if len(sys.argv) > 2 and sys.argv[2] == "--verbose":
+        verbose = True
+
+    if verbose:
+        print(f"Working on: {user_prompt}")
 
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
@@ -26,10 +33,14 @@ def main():
 
 
 def generate_content(client, messages):
+    global verbose
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
     )
+    if verbose:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print("Response:")
     print(response.text)
 
