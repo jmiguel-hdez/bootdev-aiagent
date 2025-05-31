6943113ac2fe204f9bb513c88f77c80abf63e8b9
jmiguel-hdez <jmiguel.hdez@gmail.com>
2025-05-31 15:11:20 -0500

    get prompt from parameters

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
 
-client = genai.Client(api_key=api_key)
-response = client.models.generate_content(
-    model="gemini-2.0-flash-001",
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
-)
+def main():
+    if len(sys.argv) < 2:
+        print("ERROR: missing prompt parameter")
+        sys.exit(1)
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
 
-if response:
-    print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    prompt_text = sys.argv[1]
+
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=prompt_text,
+    )
+
+    if response:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        print("Response:")
+        print(response.text)
+
+
+if __name__ == "__main__":
+    main()
