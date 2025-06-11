be2b5e0faf142c4cf7766d600439d0eb519a5ef9
jmiguel-hdez <jmiguel.hdez@gmail.com>
2025-06-11 13:35:00 -0500

    improve after seeing solution

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -3,25 +3,29 @@ from pathlib import Path
 
 
 def get_files_info(working_directory, directory=None):
-    # print(f"get_files_info({working_directory},{directory})")
     working_directory_abs = os.path.abspath(working_directory)
     # print(f"working_directory_abs({working_directory_abs})")
-    list_directory = working_directory_abs
+    target_directory = working_directory_abs
 
     if directory:
-        directory_abs = os.path.abspath(os.path.join(working_directory_abs, directory))
-        # print(f"directory_abs({directory_abs})")
-        if not str(directory_abs).startswith(str(working_directory_abs)):
-            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        target_directory = os.path.abspath(
+            os.path.join(working_directory_abs, directory)
+        )
 
-        if not os.path.isdir(directory_abs):
-            return f'Error: "{directory}" is not a directory'
-        list_directory = directory_abs
+    if not str(target_directory).startswith(str(working_directory_abs)):
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
 
-    file_info = []
-    for path in os.listdir(list_directory):
-        is_dir = os.path.isdir(os.path.join(list_directory, path))
-        file_size = os.path.getsize(os.path.join(list_directory, path))
-        file_info.append(f"- {path}: file_size={file_size} bytes, is_dir={is_dir}")
+    if not os.path.isdir(target_directory):
+        return f'Error: "{directory}" is not a directory'
 
-    return "\n".join(file_info)
+    try:
+        file_info = []
+        for path in os.listdir(target_directory):
+            filepath = os.path.join(target_directory, path)
+            is_dir = os.path.isdir(filepath)
+            file_size = os.path.getsize(filepath)
+            file_info.append(f"- {path}: file_size={file_size} bytes, is_dir={is_dir}")
+
+        return "\n".join(file_info)
+    except Exception as e:
+        return f"Error listing files: {e}"
