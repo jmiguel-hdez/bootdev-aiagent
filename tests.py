8947e4d372e2f977f3ad01ca93a2aa8cca2dc9c0
jmiguel-hdez <jmiguel.hdez@gmail.com>
2025-06-11 13:41:09 -0500

    improve test logic

--- tests.py
+++ tests.py
@@ -1,9 +1,19 @@
 from functions.get_files_info import get_files_info
 
+tests = [
+    ("calculator", "."),
+    ("calculator", "pkg"),
+    ("calculator", "/bin"),
+    ("calculator", "../"),
+]
+
+
+def test():
+    for workingdir, directory in tests:
+        print(f"Result for workingdir:{workingdir} directory:{directory}")
+        result = get_files_info(workingdir, directory)
+        print(result)
+
+
 if __name__ == "__main__":
-    print(get_files_info("calculator", "."))
-    print(get_files_info("calculator", "pkg"))
-    print(get_files_info("calculator", "/bin"))
-    print(get_files_info("calculator", "../"))
-    # print(get_files_info(".", "functions"))
-    # print(get_files_info("."))
+    test()
